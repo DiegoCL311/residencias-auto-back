bd7ab98cd3dc9d77398bace6216d2a7294436cec
Diego Cruz Lugo <18170307@itculiacan.edu.mx>
2022-11-12 20:53:18 -0700

    prueba de push

--- routes/reportePreliminar.js
+++ routes/reportePreliminar.js
@@ -3,7 +3,7 @@ const app = Router();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/reportePreliminar", function (req, res) {
-  res.send("Respuesta get reportePreliminar");
+  res.send("Respuesta get reportePreliminar1");
 });
 
 app.post("/reportePreliminar", function (req, res) {
